e55edb0cd40c27cb51a2c8c9c7ce3f53159d5740
P J Manu Prathap <62021759+nutstacker@users.noreply.github.com>
2021-08-19 02:09:37 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,57 @@
+variable profile{
+    default = "default"
+}
+variable region{
+    default = "us-east-1"
+}
+variable s3_bucket{
+    default = "iactools"
+}
+variable state_key{
+    default = "terraform_state/terraform.tfstate"
+}
+variable dynamodb_table{
+    default = "terraform-state-lock"
+}
+variable vpc_name{
+    default = "ust_VPC"
+}
+variable subnet_name{
+    default = "ust_subnet"
+}
+variable gateway_name{
+    default = "ust_gw"
+}
+variable route_table_name{
+    default = "ust_rt"
+}
+variable eip_name{
+    default = "ust_eip"
+}
+variable network_interface_name{
+    default = "ust_ni"
+}
+variable ebs_volume_name{
+    default = "manu_volume"
+}
+variable ebs_volume_size{
+    default = "10"
+}
+variable instance_type{
+    default = "t2.micro"
+}
+variable instance_index{
+    default = "10"
+}
+variable instance_name{
+    default = "Deploy-VM"
+}
+variable route_cidr_block{
+    default = "0.0.0.0/0"
+}
+variable security_cidr_blocks{
+    default = ["0.0.0.0/0"]
+}
 variable vpc_cidr_block{
     default = "172.16.0.0/16"
 }
@@ -18,4 +72,22 @@ variable aws_subnet_id{
 }
 variable private_ip{
     default = "172.16.10.200"
+}
+variable ingress_1{
+    default = "80"
+}
+variable ingress_2{
+    default = "22"
+}
+variable ingress_3{
+    default = "443"
+}
+variable ingress_test_from{
+    default = "8081"
+}
+variable ingress_test_to{
+    default = "8089"
+}
+variable egress{
+    default = "0"
 }
